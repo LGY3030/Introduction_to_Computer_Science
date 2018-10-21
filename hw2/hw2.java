2e21bd57aac0d19070a0af723d6bc94c5c8004d7
GuanYu Lin <lgy3030@gmail.com>
2018-10-21 17:19:02 +0800

    Update

--- hw2/hw2.java
+++ hw2/hw2.java
@@ -6,6 +6,8 @@ public class hw2 {
 		int endflag=0;
 		int montha=0,monthb=0;
 		int day=0;
+		int temp1=0,temp2=0;
+		String temp3="";
 		int checkeventa=0,checkeventb=0;
 		String a="";
 		int[] eventflag = new int[32];
@@ -85,6 +87,24 @@ public class hw2 {
 				System.out.println("");
 				System.out.println("活動:");
 				if(eventflag[day]==1 && Integer.parseInt(event[day][21][0])>0) {
+					for(int i=1;i<Integer.parseInt(event[day][21][0]);i++) {
+						temp1=Integer.parseInt(event[day][i][1]);
+						temp2=Integer.parseInt(event[day][i][2]);
+						temp3=event[day][i][0];
+						for(int j=i+1;j<=Integer.parseInt(event[day][21][0]);j++){
+							if(temp1>Integer.parseInt(event[day][j][1])){
+								temp1=Integer.parseInt(event[day][j][1]);
+								temp2=Integer.parseInt(event[day][j][2]);
+								temp3=event[day][j][0];
+								event[day][j][1]=event[day][i][1];
+								event[day][j][2]=event[day][i][2];
+								event[day][j][0]=event[day][i][0];
+								event[day][i][1]=String.valueOf(temp1);
+								event[day][i][2]=String.valueOf(temp2);
+								event[day][i][0]=temp3;
+							}
+						}						
+					}
 					for(int i=1;i<=Integer.parseInt(event[day][21][0]);i++) {
 						System.out.printf("%d.",i);
 						System.out.println(event[day][i][0]);
@@ -93,7 +113,16 @@ public class hw2 {
 				System.out.println("");
 				System.out.println("輸入指令:");
 				a=scanner.nextLine();
-				if(a.substring(0,3).equals("add")) {
+				if(a.equals("exit")) {
+					break;
+				}
+				else if(a.equals("month")) {
+					switchflag=0;
+				}
+				else if(a.substring(0,3).equals("del")) {
+					
+				}
+				else if(a.substring(0,3).equals("add")) {
 					if(Integer.parseInt(event[day][21][0])==0){
 						checkeventa=60*Integer.parseInt(a.substring(4,6))+Integer.parseInt(a.substring(7,9));
 						checkeventb=60*Integer.parseInt(a.substring(10,12))+Integer.parseInt(a.substring(13,15));
@@ -125,6 +154,7 @@ public class hw2 {
 								event[day][Integer.parseInt(event[day][21][0])][0]=a.substring(4,15)+" "+a.substring(16);
 								event[day][Integer.parseInt(event[day][21][0])][1]=String.valueOf(checkeventa);
 								event[day][Integer.parseInt(event[day][21][0])][2]=String.valueOf(checkeventb);
+								break;
 							}
 						}
 					}
