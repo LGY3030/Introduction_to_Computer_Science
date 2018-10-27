100e820475ac2fa0584b5132a93a7e222a634359
GuanYu Lin <lgy3030@gmail.com>
2018-10-27 15:31:51 +0800

    Update

--- hw2/hw2.java
+++ hw2/hw2.java
@@ -186,33 +186,42 @@ public class hw2 {
 				}
 
 				else if(a.substring(0,4).equals("edit")) {        //若輸入edit-[活動編號]-[活動時間]，則編輯現有activity
-					for(int i=1;i<=Integer.parseInt(event[day][21][0]);i++) {
 
-						checkeventa=60*Integer.parseInt(a.substring(7,9))+Integer.parseInt(a.substring(10,12));        //把開始的時跟分轉換成用分表示
-						checkeventb=60*Integer.parseInt(a.substring(13,15))+Integer.parseInt(a.substring(16,18));        //把結束的時跟分轉換成用分表示
-
-						//檢查欲改變的時間是否有跟其他activity衝突
-						if(checkeventa>=Integer.parseInt(event[day][i][1])&&checkeventa<=Integer.parseInt(event[day][i][2])) {
-							activitycheck=activitycheck+1;
-						}
-						else if(checkeventb>=Integer.parseInt(event[day][i][1])&&checkeventb<=Integer.parseInt(event[day][i][2])){
-							activitycheck=activitycheck+1;
-						}
-						else if(checkeventa<=Integer.parseInt(event[day][i][1])&&checkeventb>=Integer.parseInt(event[day][i][2])){
-							activitycheck=activitycheck+1;
-						}
+					//指令時間與現實時間不符合
+					if(Integer.parseInt(a.substring(7,9))<0||Integer.parseInt(a.substring(7,9))>23||Integer.parseInt(a.substring(13,15))<0||Integer.parseInt(a.substring(13,15))>23||Integer.parseInt(a.substring(10,12))<0||Integer.parseInt(a.substring(10,12))>59||Integer.parseInt(a.substring(16,18))<0||Integer.parseInt(a.substring(16,18))>59){
+						System.out.println("指令時間與現實不符");
 					}
+					
+					//指令時間與現實時間符合
+					else{
+						for(int i=1;i<=Integer.parseInt(event[day][21][0]);i++) {
 
-						//若activitycheck為0，代表沒有衝突，可以編輯，若不為0，則有衝突，不可編輯
-						if(activitycheck>0){
-							System.out.println("該時間區段已有活動");
+							checkeventa=60*Integer.parseInt(a.substring(7,9))+Integer.parseInt(a.substring(10,12));        //把開始的時跟分轉換成用分表示
+							checkeventb=60*Integer.parseInt(a.substring(13,15))+Integer.parseInt(a.substring(16,18));        //把結束的時跟分轉換成用分表示
+
+							//檢查欲改變的時間是否有跟其他activity衝突
+							if(checkeventa>=Integer.parseInt(event[day][i][1])&&checkeventa<Integer.parseInt(event[day][i][2])) {
+								activitycheck=activitycheck+1;
+							}
+							else if(checkeventb>Integer.parseInt(event[day][i][1])&&checkeventb<=Integer.parseInt(event[day][i][2])){
+								activitycheck=activitycheck+1;
+							}
+							else if(checkeventa<=Integer.parseInt(event[day][i][1])&&checkeventb>=Integer.parseInt(event[day][i][2])){
+								activitycheck=activitycheck+1;
+							}
 						}
-						else{
-							event[day][Integer.parseInt(a.substring(5,6))][0]=a.substring(7,9)+":"+a.substring(10,12)+"~"+a.substring(13,15)+":"+a.substring(16,18)+" "+event[day][Integer.parseInt(a.substring(5,6))][0].substring(12);        //新的activity
-							event[day][Integer.parseInt(a.substring(5,6))][1]=String.valueOf(checkeventa);        //新的開始時間
-							event[day][Integer.parseInt(a.substring(5,6))][2]=String.valueOf(checkeventb);        //新的結束時間
-						}
-						activitycheck=0;        //每次檢查完，要把activitycheck設為0，下次檢查才會是對的
+
+							//若activitycheck為0，代表沒有衝突，可以編輯，若不為0，則有衝突，不可編輯
+							if(activitycheck>0){
+								System.out.println("該時間區段已有活動");
+							}
+							else{
+								event[day][Integer.parseInt(a.substring(5,6))][0]=a.substring(7,9)+":"+a.substring(10,12)+"~"+a.substring(13,15)+":"+a.substring(16,18)+" "+event[day][Integer.parseInt(a.substring(5,6))][0].substring(12);        //新的activity
+								event[day][Integer.parseInt(a.substring(5,6))][1]=String.valueOf(checkeventa);        //新的開始時間
+								event[day][Integer.parseInt(a.substring(5,6))][2]=String.valueOf(checkeventb);        //新的結束時間
+							}
+							activitycheck=0;        //每次檢查完，要把activitycheck設為0，下次檢查才會是對的
+					}
 				}
 
 				else if(a.substring(0,3).equals("del")) {        //若前3個字是輸入del，進入此迴圈
@@ -250,49 +259,56 @@ public class hw2 {
 				}
 
 				else if(a.substring(0,3).equals("add")) {        //若輸入add-[活動時間]-[活動名稱]，則增加新的activity
-					
-					//若當日原本沒有activity，則直接加入activity
-					if(Integer.parseInt(event[day][21][0])==0){
-						checkeventa=60*Integer.parseInt(a.substring(4,6))+Integer.parseInt(a.substring(7,9));        //把開始的時跟分轉換成用分表示
-						checkeventb=60*Integer.parseInt(a.substring(10,12))+Integer.parseInt(a.substring(13,15));        //把結束的時跟分轉換成用分表示
-						event[day][21][0]=String.valueOf(Integer.parseInt(event[day][21][0])+1);        //activity數量加1
-						eventflag[day]=1;
-						event[day][Integer.parseInt(event[day][21][0])][0]=a.substring(4,15)+" "+a.substring(16);        //activity
-						event[day][Integer.parseInt(event[day][21][0])][1]=String.valueOf(checkeventa);        //開始時間
-						event[day][Integer.parseInt(event[day][21][0])][2]=String.valueOf(checkeventb);        //結束時間
+
+					//指令時間與現實時間不符合
+					if(Integer.parseInt(a.substring(4,6))<0||Integer.parseInt(a.substring(4,6))>23||Integer.parseInt(a.substring(10,12))<0||Integer.parseInt(a.substring(10,12))>23||Integer.parseInt(a.substring(7,9))<0||Integer.parseInt(a.substring(7,9))>59||Integer.parseInt(a.substring(13,15))<0||Integer.parseInt(a.substring(13,15))>59){
+						System.out.println("指令時間與現實不符");
 					}
 
-					//若當日已有activity，則要確認欲加入的activity沒有和原本的acivity時間衝突
+					//指令時間與現實時間符合
 					else{
-						for(int i=1;i<=Integer.parseInt(event[day][21][0]);i++) {
+						//若當日原本沒有activity，則直接加入activity
+						if(Integer.parseInt(event[day][21][0])==0){
 							checkeventa=60*Integer.parseInt(a.substring(4,6))+Integer.parseInt(a.substring(7,9));        //把開始的時跟分轉換成用分表示
 							checkeventb=60*Integer.parseInt(a.substring(10,12))+Integer.parseInt(a.substring(13,15));        //把結束的時跟分轉換成用分表示
-
-							//檢查欲加入的activity是否有跟其他activity衝突
-							if(checkeventa>=Integer.parseInt(event[day][i][1])&&checkeventa<=Integer.parseInt(event[day][i][2])) {
-								activitycheck=activitycheck+1;
-							}
-							else if(checkeventb>=Integer.parseInt(event[day][i][1])&&checkeventb<=Integer.parseInt(event[day][i][2])){
-								activitycheck=activitycheck+1;
-							}
-							else if(checkeventa<=Integer.parseInt(event[day][i][1])&&checkeventb>=Integer.parseInt(event[day][i][2])){
-								activitycheck=activitycheck+1;
-							}
-						}
-
-						//若activitycheck為0，代表沒有衝突，可以增加，若不為0，則有衝突，不可增加
-						if(activitycheck>0){
-							System.out.println("該時間區段已有活動");
-						}
-						else{
 							event[day][21][0]=String.valueOf(Integer.parseInt(event[day][21][0])+1);        //activity數量加1
 							eventflag[day]=1;
-							event[day][Integer.parseInt(event[day][21][0])][0]=a.substring(4,15)+" "+a.substring(16);
+							event[day][Integer.parseInt(event[day][21][0])][0]=a.substring(4,15)+" "+a.substring(16);        //activity
 							event[day][Integer.parseInt(event[day][21][0])][1]=String.valueOf(checkeventa);        //開始時間
 							event[day][Integer.parseInt(event[day][21][0])][2]=String.valueOf(checkeventb);        //結束時間
 						}
-						activitycheck=0;        //每次檢查完，要把activitycheck設為0，下次檢查才會是對的
 
+						//若當日已有activity，則要確認欲加入的activity沒有和原本的acivity時間衝突
+						else{
+							for(int i=1;i<=Integer.parseInt(event[day][21][0]);i++) {
+								checkeventa=60*Integer.parseInt(a.substring(4,6))+Integer.parseInt(a.substring(7,9));        //把開始的時跟分轉換成用分表示
+								checkeventb=60*Integer.parseInt(a.substring(10,12))+Integer.parseInt(a.substring(13,15));        //把結束的時跟分轉換成用分表示
+
+								//檢查欲加入的activity是否有跟其他activity衝突
+								if(checkeventa>=Integer.parseInt(event[day][i][1])&&checkeventa<Integer.parseInt(event[day][i][2])) {
+									activitycheck=activitycheck+1;
+								}
+								else if(checkeventb>Integer.parseInt(event[day][i][1])&&checkeventb<=Integer.parseInt(event[day][i][2])){
+									activitycheck=activitycheck+1;
+								}
+								else if(checkeventa<=Integer.parseInt(event[day][i][1])&&checkeventb>=Integer.parseInt(event[day][i][2])){
+									activitycheck=activitycheck+1;
+								}
+							}
+
+							//若activitycheck為0，代表沒有衝突，可以增加，若不為0，則有衝突，不可增加
+							if(activitycheck>0){
+								System.out.println("該時間區段已有活動");
+							}
+							else{
+								event[day][21][0]=String.valueOf(Integer.parseInt(event[day][21][0])+1);        //activity數量加1
+								eventflag[day]=1;
+								event[day][Integer.parseInt(event[day][21][0])][0]=a.substring(4,15)+" "+a.substring(16);
+								event[day][Integer.parseInt(event[day][21][0])][1]=String.valueOf(checkeventa);        //開始時間
+								event[day][Integer.parseInt(event[day][21][0])][2]=String.valueOf(checkeventb);        //結束時間
+							}
+							activitycheck=0;        //每次檢查完，要把activitycheck設為0，下次檢查才會是對的
+						}
 					}
 					
 				}
