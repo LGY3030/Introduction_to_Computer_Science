451ae4360c87adfdefb172595d61c60eccb85d45
GuanYu Lin <lgy3030@gmail.com>
2018-12-02 00:47:39 +0800

    Update

--- hw4/hw4.java
+++ hw4/hw4.java
@@ -4,9 +4,10 @@ public class hw4 {
 	
 	public static void main(String[] args) {
 		
-		int round=0;
+		int roundflag=1;
 		int turn=0;
 		String[] getname;
+		String instruction;
 		player player1=new player("player1");
 		player player2=new player("player2");
 		Scanner scanner=new Scanner(System.in);
@@ -14,8 +15,22 @@ public class hw4 {
 		getname=scanner.next().split("]");
 		player1.name=getname[0].substring(1);
 		player2.name=getname[1].substring(1);
-		for(int i=0;i<15;i++) {
-			System.out.println(player1.allcards.get(i));
+		while(true) {
+			instruction=scanner.next();
+			if(instruction.substring(0,4).equals("exit")) {
+				break;
+			}
+			else if(instruction.substring(0,4).equals("fini")) {
+				if(roundflag==1) {
+					roundflag=2;
+				}
+				else {
+					roundflag=1;
+				}
+			}
+			else if(instruction.substring(0,4).equals("sele")) {
+				
+			}
 		}
 	}
 }
